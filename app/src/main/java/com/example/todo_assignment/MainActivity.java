5714dc998b5b5e8dcb7993a47b8bb7106286b859
Somesh Patel <56200611+im-somax@users.noreply.github.com>
2020-09-14 16:56:07 +0530

    First commit

---
+++ app/src/main/java/com/example/todo_assignment/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.todo_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView titlepage, subtitlepage, endpage;
+    Button btnAddNew;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference notebookRef = db.collection("Notebook");
+    private DoesAdapter doesAdapter;
+    FirebaseFirestore reference;
+    RecyclerView projectList;
+    ArrayList<MyDoes> list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        titlepage = findViewById(R.id.titlepage);
+        subtitlepage = findViewById(R.id.subtitlepage);
+        endpage = findViewById(R.id.endpage);
+
+        btnAddNew = findViewById(R.id.btnAddNew);
+
+        // import font
+        Typeface MLight = Typeface.createFromAsset(getAssets(), "fonts/ML.ttf");
+        Typeface MMedium = Typeface.createFromAsset(getAssets(), "fonts/MM.ttf");
+
+        // customize font
+        titlepage.setTypeface(MMedium);
+        subtitlepage.setTypeface(MLight);
+        endpage.setTypeface(MLight);
+
+        btnAddNew.setTypeface(MLight);
+
+        btnAddNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, NewTaskActivity.class));
+            }
+        });
+        setUpRecyclerView();
+    }
+    private void setUpRecyclerView() {
+        Query query = notebookRef.orderBy("priority", Query.Direction.DESCENDING);
+        FirestoreRecyclerOptions<MyDoes> options = new FirestoreRecyclerOptions.Builder<MyDoes>()
+                .setQuery(query, MyDoes.class)
+                .build();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+    }
+}
---
+++ app/src/main/java/com/example/todo_assignment/NewProject.java
@@ -0,0 +1,14 @@
+package com.example.todo_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewProject extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_project);
+    }
+}
